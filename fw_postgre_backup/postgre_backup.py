6caab3f2a24fa23acf43966dc81abe8e56084592
FairWind <sergey.manenok@gmail.com>
2023-09-14 19:42:31 +0300

    add control checks for mount devices

--- fw_postgre_backup/postgre_backup.py
+++ fw_postgre_backup/postgre_backup.py
@@ -10,9 +10,10 @@ if __name__ == "__main__":
             '//bkp0201.bs.local.erc/postgresql': '/mnt/bkp0201',
             '//bkp0101.bs.local.erc/postgresql': '/mnt/bkp0101',
         },
-        'destination_dirs': ['/mnt/bkp0201', '/mnt/bkp0101']
+        #'destination_dirs': ['/mnt/bkp0201', '/mnt/bkp0101']
     }
     logging.getLogger().setLevel(logging.DEBUG)
+    destination_dirs = backup_config.get('destination_dirs', [])
     mounter_controls = []
     if 'mount_points' in backup_config:
         for device, mount_point in backup_config['mount_points'].items():
@@ -20,6 +21,7 @@ if __name__ == "__main__":
             if not mounter.check_mount():
                 mounter.mount()
             if mounter.check_mount():
+                destination_dirs.append(mounter.mount_point)
                 mounter_controls.append(mounter)
 
     # for mounter in mounter_controls:
