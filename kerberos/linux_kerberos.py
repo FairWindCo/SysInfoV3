64373f4480943981e170e7dda2cfdd5e94d5551d
FairWind <sergey.manenok@gmail.com>
2023-09-14 18:56:00 +0300

    first stage backup script

--- kerberos/linux_kerberos.py
+++ kerberos/linux_kerberos.py
@@ -5,5 +5,5 @@ def init_key(keytab_file: str = "/etc/krb5.keytab", user_realm: str = None, in_s
     kinit_args = ['-kt', keytab_file]
     if user_realm:
         kinit_args.append(user_realm)
-    result, *_ = execute_os_command('/usr/bin/kinit', kinit_args, in_sudo=in_sudo)
+    result, *_ = execute_os_command('/usr/bin/kinit', *kinit_args, in_sudo=in_sudo)
     return result
