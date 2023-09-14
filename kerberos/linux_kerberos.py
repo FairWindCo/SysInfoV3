d6321a8e4bfb69ea6d1201aaaf0c74d210af117d
FairWind <sergey.manenok@gmail.com>
2023-09-14 17:36:48 +0300

    add kerberos init key to MountControl

--- kerberos/linux_kerberos.py
+++ kerberos/linux_kerberos.py
@@ -1,9 +1,9 @@
 from fw_utils.mounting import execute_os_command
 
 
-def init_key(keytab_file: str = "/etc/krb5.keytab", user_realm: str = None):
+def init_key(keytab_file: str = "/etc/krb5.keytab", user_realm: str = None, in_sudo: bool = True):
     kinit_args = ['-kt', keytab_file]
     if user_realm:
         kinit_args.append(user_realm)
-    result, *_ = execute_os_command('/usr/bin/kinit', kinit_args, in_sudo=True)
+    result, *_ = execute_os_command('/usr/bin/kinit', kinit_args, in_sudo=in_sudo)
     return result
