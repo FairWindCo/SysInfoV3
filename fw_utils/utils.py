41d1e16b9859e287951ad40df20c736644559dfe
FairWind <sergey.manenok@gmail.com>
2023-09-15 16:02:57 +0300

    try fix execute command with pypy in bash

--- fw_utils/utils.py
+++ fw_utils/utils.py
@@ -68,8 +68,11 @@ def execute_os_command(*commands: str, in_sudo: bool = True, has_pipe: bool = Fa
     if has_pipe:
         #command_for_execute.append('--')
         command_for_execute.append('/usr/bin/sh')
-        command_for_execute.append('-c')
-        command_for_execute.append(f"\'{' '.join(commands)}\'")
+        #command_for_execute.append('-c')
+        #command_for_execute.append(f"\'{' '.join(commands)}\'")
+        command_for_execute.append("<<'SCRIPT'")
+        command_for_execute.extend(commands)
+        command_for_execute.append("SCRIPT")
         #command_for_execute.append(' '.join(commands))
     else:
         command_for_execute.extend(commands)
