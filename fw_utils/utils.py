06dd4cc3816000cf6ca32b5d5386232624a1f03f
FairWind <sergey.manenok@gmail.com>
2023-09-15 16:11:06 +0300

    test execute in sudo and shell

--- fw_utils/utils.py
+++ fw_utils/utils.py
@@ -69,8 +69,8 @@ def execute_os_command(*commands: str, in_sudo: bool = True, has_pipe: bool = Fa
         #command_for_execute.append('--')
         command_for_execute.append('/usr/bin/sh')
         command_for_execute.append('-c')
-        command_for_execute.append(f"\'{' '.join(commands)}\'")
-        #command_for_execute.append(' '.join(commands))
+        #command_for_execute.append(f"\'{' '.join(commands)}\'")
+        command_for_execute.append(' '.join(commands))
     else:
         command_for_execute.extend(commands)
     # logging.debug(f'ARGS: {command_for_execute} ')
