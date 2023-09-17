38110c41af29d9c7365d71e42d2294b25df0f6f8
FairWind <sergey.manenok@gmail.com>
2023-09-18 00:23:33 +0300

    add soft info

--- fw_linux_sysinfo/host_info.py
+++ fw_linux_sysinfo/host_info.py
@@ -127,7 +127,8 @@ def get_host_info():
                 soft_info = data.split(' ')
                 if soft_info:
                     sys_info['soft'].append({
-                        'name': soft_info[0].strip('/')[0]
+                        'name': soft_info[0].strip('/')[0],
+                        'version': soft_info[1]
                     })
 
     else:
