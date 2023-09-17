9b0a96cf93c7e72566a80686e02654b484f619f2
FairWind <sergey.manenok@gmail.com>
2023-09-18 00:35:13 +0300

    add soft info

--- fw_linux_sysinfo/host_info.py
+++ fw_linux_sysinfo/host_info.py
@@ -125,8 +125,8 @@ def get_host_info():
                     and not data.startswith('python3-')
                     and data.find('-dev') == -1):
                 soft_info = data.split(' ')
-                print(soft_info)
-                if soft_info and len(sys_info) > 2:
+                if soft_info and len(sys_info) > 1:
+                    print(soft_info)
                     ver_ubuntu_index = soft_info[1].find('ubuntu')
                     sys_info['soft'].append({
                         'version': soft_info[1][:ver_ubuntu_index] if ver_ubuntu_index > 0 else soft_info[1],
